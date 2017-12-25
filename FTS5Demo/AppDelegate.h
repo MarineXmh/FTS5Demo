30cea6e8a65605d6e4964ed7739bfa14a3425c59
xumenghua <xumenghua@syswin.com>
2017-12-25 11:17:40 +0800

    Initial Commit

---
+++ FTS5Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FTS5Demo
+//
+//  Created by Xu Menghua on 2017/12/25.
+//  Copyright © 2017年 Xu Menghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
